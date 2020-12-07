13b1e8197158052d3ecddf6840bbc7031586afcd
Rachel Levinson <rachel.a.levinson@gmail.com>
2020-12-07 20:30:31 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,27 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    count_array = self.split(/\.|\?|\!/)
+    count_array.count do |x|
+      x.size >= 1
+    # binding.pry
+    end
+    #SPLIT by any last punctuation into an array
+    #COMPACT(!) removes nil values from an array
+      # - didn't need to use because we're qualifying based on content existing
+    #COUNT counts number of items in array
+    #REGEX?
   end
 end
